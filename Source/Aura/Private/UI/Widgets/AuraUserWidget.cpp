69eb9981172509434c5d8f0f9f8ff778b0b1b7bf
patrickordine <patrick_ordine@yahoo.com.br>
2024-11-14 08:33:36 -0300

    Globe ProgressBar Widget Base

---
+++ Source/Aura/Private/UI/Widgets/AuraUserWidget.cpp
@@ -0,0 +1,11 @@
+// Copyright Druid Mechanics
+
+
+#include "UI/Widgets/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+
+}
